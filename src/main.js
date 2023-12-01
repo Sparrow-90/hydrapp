323b68b5049700969dfebcba8ffe747f8fd8fc6f
Mateusz Wróbel <mw.animacja@gmail.com>
2023-12-01 13:40:37 +0100

    add detail to style and setup js

---
+++ src/main.js
@@ -0,0 +1,12 @@
+const counter = document.querySelector('.counter--js');
+const addBtn = document.querySelector('.add--js');
+const removeBtn= document.querySelector('.remove--js');
+
+
+addBtn.addEventListener('click', ()=>{
+    console.log('działa')
+})
+
+removeBtn.addEventListener('click', ()=>{
+    console.log('remove działa')
+})
